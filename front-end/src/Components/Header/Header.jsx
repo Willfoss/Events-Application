350d3385e10777146d1619528536b72b099f266e
Willfoss <118803774+Willfoss@users.noreply.github.com>
2025-02-04 14:22:12 +0000

    added basic header for login/signup

---
+++ front-end/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState } from "react";
+import { Calendar1, Search } from "lucide-react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <section id="header">
+      <Calendar1 className="logo"></Calendar1>
+      <h1 className="logo-heading">EventSphere</h1>
+    </section>
+  );
+}
