def549d77550af40892ef29f75c9d9c5d0b48804
Willfoss <118803774+Willfoss@users.noreply.github.com>
2025-02-06 13:59:43 +0000

    added testin data for back-end

---
+++ back-end/db/data/test-data/attendees.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    eventId: 5,
+    email: "willfossard@outlook.com",
+    name: "Will Fossard",
+  },
+  {
+    eventId: 5,
+    email: "usertestemail1@email.com",
+    name: "usertest1",
+  },
+  {
+    eventId: 5,
+    email: "usertestemail2@email.com",
+    name: "usertest2",
+  },
+  {
+    eventId: 5,
+    email: "usertestemail3@email.com",
+    name: "usertest3",
+  },
+  {
+    eventId: 5,
+    email: "usertestemail4@email.com",
+    name: "usertest4",
+  },
+  {
+    eventId: 5,
+    email: "usertestemail4@email.com",
+    name: "usertest4",
+  },
+  {
+    eventId: 4,
+    email: "usertestemail1@email.com",
+    name: "usertest1",
+  },
+  {
+    eventId: 4,
+    email: "usertestemail2@email.com",
+    name: "usertest2",
+  },
+  {
+    eventId: 4,
+    email: "usertestemail3@email.com",
+    name: "usertest3",
+  },
+  {
+    eventId: 3,
+    email: "usertestemail1@email.com",
+    name: "usertest1",
+  },
+  {
+    eventId: 3,
+    email: "usertestemail2@email.com",
+    name: "usertest2",
+  },
+  {
+    eventId: 2,
+    email: "usertestemail2@email.com",
+    name: "usertest2",
+  },
+];
