f6ce436842e48e9f19bf2f66d1cdf849a3abd5a8
Willfoss <118803774+Willfoss@users.noreply.github.com>
2025-02-12 09:46:14 +0000

    added basic api routing

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
-
+const cors = require("cors");
 const { serverErrorHandler } = require("./error-handling");
+const apiRouter = require("./routes/api-router");
 
 const app = express();
+app.use(cors());
+
+app.get("/", (request, response) => {
+  response.send("API ONLINE");
+});
+
+app.use("/api", apiRouter);
+
+app.all("*", (response, request) => {
+  response.status(404).send({ message: "this path does not exist" });
+});
 
 app.use(serverErrorHandler);
 
