2a4312dd9b10b9f890f799fc1542037be94851da
Willfoss <118803774+Willfoss@users.noreply.github.com>
2025-02-18 14:47:50 +0000

    updated users data

--- back-end/db/data/development-data/users.js
+++ back-end/db/data/development-data/users.js
@@ -1,42 +1,50 @@
 const bcrypt = require("bcrypt");
 
-const usersData = [
-  {
-    email: "willfossard@outlook.com",
-    name: "Will Fossard",
-    password: bcrypt.hashSync("password123", 10),
-    role: "admin",
-  },
-  {
-    email: "usertestemail1@email.com",
-    name: "usertest1",
-    password: bcrypt.hashSync("password1234", 10),
-    role: "user",
-  },
-  {
-    email: "usertestemail2@email.com",
-    name: "usertest2",
-    password: bcrypt.hashSync("password12345", 10),
-    role: "user",
-  },
-  {
-    email: "usertestemail3@email.com",
-    name: "usertest3",
-    password: bcrypt.hashSync("password123456", 10),
-    role: "user",
-  },
-  {
-    email: "usertestemail4@email.com",
-    name: "usertest4",
-    password: bcrypt.hashSync("password1234567", 10),
-    role: "staff",
-  },
-  {
-    email: "usertestemail5@email.com",
-    name: "usertest5",
-    password: bcrypt.hashSync("password12345678", 10),
-    role: "staff",
-  },
-];
+usersData = {
+  users: [
+    {
+      email: "willfossard@outlook.com",
+      name: "Will Fossard",
+      password: bcrypt.hashSync("password123", 10),
+      role: "admin",
+    },
+    {
+      email: "usertestemail1@email.com",
+      name: "usertest1",
+      password: bcrypt.hashSync("password1234", 10),
+      role: "user",
+    },
+    {
+      email: "usertestemail2@email.com",
+      name: "usertest2",
+      password: bcrypt.hashSync("password12345", 10),
+      role: "user",
+    },
+    {
+      email: "usertestemail3@email.com",
+      name: "usertest3",
+      password: bcrypt.hashSync("password123456", 10),
+      role: "user",
+    },
+    {
+      email: "usertestemail4@email.com",
+      name: "usertest4",
+      password: bcrypt.hashSync("password1234567", 10),
+      role: "staff",
+    },
+    {
+      email: "usertestemail5@email.com",
+      name: "usertest5",
+      password: bcrypt.hashSync("password12345678", 10),
+      role: "staff",
+    },
+    {
+      email: "usertestemail6@email.com",
+      name: "usertest6",
+      password: bcrypt.hashSync("password123456789", 10),
+      role: "user",
+    },
+  ],
+};
 
 module.exports = { usersData };
