4d3548dcb8e6a3ef89accfffc211eb5741f95159
Willfoss <118803774+Willfoss@users.noreply.github.com>
2025-02-21 12:13:44 +0000

    single event component finished

--- front-end/src/Components/SingleEvent/SingleEvent.jsx
+++ front-end/src/Components/SingleEvent/SingleEvent.jsx
@@ -3,15 +3,25 @@ import "./singleEvent.css";
 import UserHeader from "../UserHeader/UserHeader";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../../Context/UserContext";
-import { getSingleEvent } from "../../api";
+import { deleteEventAttendee, getAttendeesForEvent, getSingleEvent, registerForEvent } from "../../api";
 import setAuthorisationHeader from "../../Auth/auth-config";
 import { convertToLongStringMonth } from "../utils/utils";
+import buttonLoading from "../../assets/loading-button.json";
+import Lottie from "lottie-react";
+import Toast from "../Toast/Toast";
+import Loading from "../Loading/Loading";
 
 export default function SingleEvent() {
   const { event_id } = useParams();
   const [isLoading, setIsLoading] = useState(false);
+  const [isRegistrationLoading, setIsRegistrationLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [ErrorMessage, setErrorMessage] = useState("");
+  const [showErrorToast, setShowErrorToast] = useState(false);
+  const [isCancellingLoading, setIsCancellingLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isUserAlreadyAttendingEvent, setIsUserAlreadyAttendingEvent] = useState(false);
+  const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [toastSuccessMessage, setToastSuccessMessage] = useState("");
   const [event, setEvent] = useState({});
   const { loggedInUser } = useContext(UserContext);
   const [startDay, setStartDay] = useState("");
@@ -20,6 +30,7 @@ export default function SingleEvent() {
   const [endDay, setEndDay] = useState("");
   const [endMonth, setEndMonth] = useState("");
   const [endYear, setEndYear] = useState("");
+  let [optimisticAttendeeCount, setOptimisticAttendeeCount] = useState(0);
 
   useEffect(() => {
     setIsLoading(true);
@@ -34,57 +45,137 @@ export default function SingleEvent() {
         setEndDay(event.end_date.slice(0, 2));
         setEndMonth(convertToLongStringMonth(event.end_date.slice(3, 5)));
         setEndYear(event.end_date.slice(6, 11));
+        setOptimisticAttendeeCount(event.number_of_attendees);
+
+        return getAttendeesForEvent(event.event_id, authorisation);
+      })
+      .then((attendees) => {
+        attendees.forEach((attendee) => {
+          if (attendee.user_id === loggedInUser.user_id) {
+            setIsUserAlreadyAttendingEvent(true);
+          }
+        });
         setIsLoading(false);
       })
       .catch((error) => {
         setIsLoading(false);
         setIsError(true);
+        showErrorToast(true);
         setErrorMessage(error.response.data.message);
       });
   }, [event_id]);
 
-  function registerForEvent() {}
+  function signupForEvent() {
+    setIsRegistrationLoading(true);
+    setIsError(false);
+    const authorisation = setAuthorisationHeader(loggedInUser);
+    setOptimisticAttendeeCount((optimisticAttendeeCount += 1));
+    registerForEvent(event.event_id, loggedInUser.user_id, authorisation)
+      .then(({ attendee }) => {
+        setIsRegistrationLoading(false);
+        setIsUserAlreadyAttendingEvent(true);
+        setShowSuccessToast(true);
+        setToastSuccessMessage(`You have successfully signed up to ${event.event_title}`);
+      })
+      .catch((error) => {
+        setIsError(true);
+        showErrorToast(true);
+        setIsRegistrationLoading(false);
+        setOptimisticAttendeeCount((optimisticAttendeeCount -= 1));
 
-  console.log(event);
+        setErrorMessage(error.response.data.message);
+      });
+  }
+
+  function cancelRegistration() {
+    setIsCancellingLoading(true);
+    setIsError(false);
+    setOptimisticAttendeeCount((optimisticAttendeeCount -= 1));
+    const authorisation = setAuthorisationHeader(loggedInUser);
+    deleteEventAttendee(event.event_id, loggedInUser.user_id, authorisation)
+      .then(() => {
+        setIsCancellingLoading(false);
+        setIsUserAlreadyAttendingEvent(false);
+        setShowSuccessToast(true);
+        setToastSuccessMessage(`You are no longer attending ${event.event_title}`);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+        setIsCancellingLoading(false);
+        setIsError(true);
+        showErrorToast(true);
+        setOptimisticAttendeeCount((optimisticAttendeeCount += 1));
+        setErrorMessage(error.response.data.message);
+      });
+  }
 
   return (
     <div className="single-event">
       <UserHeader />
-      <section id="single-event-container">
-        <img className="single-event-image" src={event.image}></img>
-        <div className="event-content-container">
-          <h2 className="event-title">{event.event_title}</h2>
-          <p className="event-text">{event.event_description}</p>
-          <p className="event-text">
-            <span className="bold">Organiser: </span>
-            {event.host}
-          </p>
-          <p className="event-text">
-            <span className="bold">Start Date: </span>
-            {startDay} {startMonth} {startYear}
-          </p>
-          <p className="event-text">
-            <span className="bold">End Date: </span>
-            {endDay} {endMonth} {endYear}
-          </p>
-          <p className="event-text">
-            <span className="bold">Start Time: </span> {event.start_time}
-          </p>
-          <p className="event-text">
-            <span className="bold">End Time: </span> {event.end_time}
-          </p>
-          <p className="event-text">
-            <span className="bold">Number of attendees: </span> {event.number_of_attendees}
-          </p>
-          <p className="event-text">
-            <span className="bold">Location: </span>
-            {event.location}
-          </p>
-          <button className="register-for-event-button" onClick={registerForEvent}>
-            Register for Event
-          </button>
-        </div>
-      </section>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          {showSuccessToast && <Toast setShowToast={setShowSuccessToast} success="yes" successMessage={toastSuccessMessage} />}
+          {showErrorToast && <Toast error="yes" setShowToast={setShowErrorToast} errorMessage={errorMessage} />}
+          <section id="single-event-container">
+            <img className="single-event-image" src={event.image}></img>
+            <div className="event-content-container">
+              <h2 className="event-title">{event.event_title}</h2>
+              <p className="event-text">{event.event_description}</p>
+              <p className="event-text">
+                <span className="bold">Organiser: </span>
+                {event.host}
+              </p>
+              <p className="event-text">
+                <span className="bold">Start Date: </span>
+                {startDay} {startMonth} {startYear}
+              </p>
+              <p className="event-text">
+                <span className="bold">End Date: </span>
+                {endDay} {endMonth} {endYear}
+              </p>
+              <p className="event-text">
+                <span className="bold">Start Time: </span> {event.start_time}
+              </p>
+              <p className="event-text">
+                <span className="bold">End Time: </span> {event.end_time}
+              </p>
+              <p className="event-text">
+                <span className="bold">Number of attendees: </span> {optimisticAttendeeCount}
+              </p>
+              <p className="event-text">
+                <span className="bold">Location: </span>
+                {event.location}
+              </p>
+              {!isUserAlreadyAttendingEvent ? (
+                isRegistrationLoading ? (
+                  <div className="registration-button-loading">
+                    <Lottie className="button-loading-animation " animationData={buttonLoading} loop={true} />
+                  </div>
+                ) : (
+                  <button className="register-for-event-button" onClick={signupForEvent}>
+                    Register for Event
+                  </button>
+                )
+              ) : (
+                <div className="cancel-reg-container">
+                  <div className="registered-for-event">You are already signed up for this event!</div>
+                  {isCancellingLoading ? (
+                    <div className="cancel-registration">
+                      <Lottie className="button-loading-animation " animationData={buttonLoading} loop={true} />
+                    </div>
+                  ) : (
+                    <div className="cancel-registration" onClick={cancelRegistration}>
+                      Cancel Registration
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          </section>
+        </>
+      )}
     </div>
   );
 }
--- front-end/src/api.js
+++ front-end/src/api.js
@@ -27,3 +27,19 @@ export function getSingleEvent(event_id, authorisation) {
     return data;
   });
 }
+
+export function registerForEvent(event_id, user_id, authorisation) {
+  return eventsphereApi.post("/attendees", { event_id, user_id }, authorisation).then(({ data }) => {
+    return data;
+  });
+}
+
+export function deleteEventAttendee(event_id, user_id, authorisation) {
+  return eventsphereApi.delete(`/attendees/${event_id}/${user_id}`, authorisation);
+}
+
+export function getAttendeesForEvent(event_id, authorisation) {
+  return eventsphereApi.get(`/attendees/${event_id}`, authorisation).then(({ data }) => {
+    return data.attendees;
+  });
+}
