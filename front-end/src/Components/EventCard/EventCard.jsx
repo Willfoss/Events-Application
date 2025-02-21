2673f5d4cd64c18434e2badab14d50080204ccc0
Willfoss <118803774+Willfoss@users.noreply.github.com>
2025-02-21 13:45:39 +0000

    updated event-card to show if user is already attending

--- front-end/src/Components/EventCard/EventCard.jsx
+++ front-end/src/Components/EventCard/EventCard.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./eventCard.css";
 import { convertToStringMonth } from "../utils/utils";
 import { Link } from "react-router-dom";
+import { getAttendeesForEvent } from "../../api";
+import { UserContext } from "../../Context/UserContext";
+import setAuthorisationHeader from "../../Auth/auth-config";
 
 export default function EventCard(props) {
   const { event } = props;
   const [windowPixels, setWindowPixels] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
+  });
+  const [isAleadyingAttending, setIsAleadyingAttending] = useState(false);
+  const { loggedInUser } = useContext(UserContext);
+
+  useState(() => {
+    const authorisation = setAuthorisationHeader(loggedInUser);
+    getAttendeesForEvent(event.event_id, authorisation).then((attendees) => {
+      attendees.forEach((attendee) => {
+        if (attendee.user_id === loggedInUser.user_id) {
+          setIsAleadyingAttending(true);
+        }
+      });
+    });
   });
 
   useEffect(() => {
@@ -23,6 +39,7 @@ export default function EventCard(props) {
 
   return (
     <section id="event-card">
+      {windowPixels.width <= 768 && isAleadyingAttending && <h3 className="attending-event-confirmation">You're signed up!</h3>}
       <div className="event-card-container">
         <div className="event-details-container">
           <img className="event-card-image" src={event.image}></img>
@@ -39,10 +56,13 @@ export default function EventCard(props) {
           </div>
         </div>
         <div className="single-article-date-container">
-          <div className="event-date-container">
-            <p className="event-text center bold">{convertToStringMonth(event.start_date.slice(3, 5))}</p>
-            <p className="event-text center bold">{event.start_date.slice(0, 2)}</p>
-            <p className="event-text center bold">{event.start_date.slice(6, 11)}</p>
+          <div className={isAleadyingAttending ? "flex-space-between" : "event-confirmation-container"}>
+            {windowPixels.width > 768 && isAleadyingAttending && <h3 className="attending-event-confirmation">You're signed up!</h3>}
+            <div className="event-date-container">
+              <p className="event-text center bold">{convertToStringMonth(event.start_date.slice(3, 5))}</p>
+              <p className="event-text center bold">{event.start_date.slice(0, 2)}</p>
+              <p className="event-text center bold">{event.start_date.slice(6, 11)}</p>
+            </div>
           </div>
           {windowPixels.width > 768 && (
             <Link to={`/events/${event.event_id}`} className="single-event-button">
